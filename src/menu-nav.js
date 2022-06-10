fd58060182929ce5962160611a94da3ecabb761c
IkanAsinn <qbin.hermawan@gmail.com>
2022-06-10 19:25:02 +0700

    ganti ke jquery

--- src/menu-nav.js
+++ src/menu-nav.js
@@ -1,20 +1,49 @@
-const sections = document.querySelectorAll('section');
-const navList = document.querySelectorAll('.navbar ul li');
+// const sections = document.querySelectorAll('section');
+// const navList = document.querySelectorAll('.navbar ul li');
 
-window.addEventListener('scroll', () => {
+// window.addEventListener('scroll', () => {
+//     let current = '';
+//     sections.forEach(section => {
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         console.log(sectionTop, sectionHeight);
+//         if (scrollY >= (sectionTop - sectionHeight / 3)) {
+//             current = section.getAttribute('id');
+//         }
+//     })
+//     navList.forEach(li => {
+//         li.classList.remove('active');
+//         if (li.classList.contains(current)) {
+//             li.classList.add('active');
+//         }
+//     })
+// })
+
+$(document).ready(() => {
+    const sections = $('section');
+    const navList = $('.navbar ul li');
     let current = '';
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        console.log(sectionTop, sectionHeight);
-        if (scrollY >= (sectionTop - sectionHeight / 3)) {
-            current = section.getAttribute('id');
+    $(window).scroll(() => {
+        let windowScroll = $(window).scrollTop();
+        sections.each((i, section) => {
+            const sectionTop = $(section).offset().top;
+            const sectionHeight = $(section).height();
+            if (windowScroll >= (sectionTop - sectionHeight / 3)) {
+                current = $(section).attr('id');
+            }
+        });
+        if (windowScroll >= $(`#${current}`).offset().top - 400) {
+            $(`#${current} .flexbox`).each((i) => {
+                setTimeout(() => {
+                    $(`#${current} .flexbox`).eq(i).addClass('pop-up');
+                }, 200 * i);
+            })
         }
-    })
-    navList.forEach(li => {
-        li.classList.remove('active');
-        if (li.classList.contains(current)) {
-            li.classList.add('active');
-        }
-    })
-})
+        navList.each((i, li) => {
+            $(li).removeClass('active');
+            if ($(li).hasClass(current)) {
+                $(li).addClass('active');
+            }
+        });
+    });
+});
