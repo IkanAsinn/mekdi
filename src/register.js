83d49f4392e7d8c444a104110357dc9bf2651797
jimash974 <jeremynchristopher@gmail.com>
2022-06-12 13:56:14 +0700

    signUp validation

---
+++ src/register.js
@@ -0,0 +1,54 @@
+let signUp = document.getElementById('register')
+let button = document.getElementById('btn-signUp')
+let phoneNum = document.getElementById('phoneNum')
+let error = document.getElementById('errorMessage')
+
+console.log(phoneNum)
+
+
+
+signUp.addEventListener('submit', function(e){
+    e.preventDefault();
+    let username = document.getElementById('name').value
+    let phoneNum = document.getElementById('phoneNum').value
+    let pass = document.getElementById('password').value
+    let confirmPass = document.getElementById('confirmPassword').value
+
+    error.style.display = "none";
+
+    // console.log(username);
+    console.log(phoneNum)
+    // console.log(pass)
+    // console.log(confirmPass)
+
+    let phoneValid = 1
+
+    for(let i = 0; i < phoneNum.length; i++){
+        if(phoneNum[i] < '0' || phoneNum[i] > '9'){
+            phoneValid = 0
+            break
+        }
+    }
+    console.log(phoneValid)
+
+    if(username.length < 5){
+        error.innerHTML = "Username must be at least 5 characters"
+        error.style.display = "block"
+        return
+    }
+    else if(phoneValid == 0){
+        error.innerHTML = "Phone Number is not valid"
+        error.style.display = "block"
+        return
+    }
+    else if(pass != confirmPass){
+        error.innerHTML = "Password is not match"
+        error.style.display = "block"
+        return
+    }
+
+    location.replace("login.html")
+})
+
+
+
